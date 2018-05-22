4bf582642b7e311358278a9fdde295c25a9f40bc
Luis <luisfnicolau@hotmail.com>
2018-05-21 21:24:48 -0300

    set cart icon

--- ColoniaFeriasWebsite/src/App.vue
+++ ColoniaFeriasWebsite/src/App.vue
@@ -1,28 +1,28 @@
 <template>
   <v-app>
     <!--<v-navigation-drawer-->
-      <!--persistent-->
-      <!--:mini-variant="miniVariant"-->
-      <!--:clipped="clipped"-->
-      <!--v-model="drawer"-->
-      <!--enable-resize-watcher-->
-      <!--fixed-->
-      <!--app-->
+    <!--persistent-->
+    <!--:mini-variant="miniVariant"-->
+    <!--:clipped="clipped"-->
+    <!--v-model="drawer"-->
+    <!--enable-resize-watcher-->
+    <!--fixed-->
+    <!--app-->
     <!--&gt;-->
-      <!--<v-list>-->
-        <!--<v-list-tile-->
-          <!--value="true"-->
-          <!--v-for="(item, i) in items"-->
-          <!--:key="i"-->
-        <!--&gt;-->
-          <!--<v-list-tile-action>-->
-            <!--<v-icon v-html="item.icon"></v-icon>-->
-          <!--</v-list-tile-action>-->
-          <!--<v-list-tile-content>-->
-            <!--<v-list-tile-title v-text="item.title"></v-list-tile-title>-->
-          <!--</v-list-tile-content>-->
-        <!--</v-list-tile>-->
-      <!--</v-list>-->
+    <!--<v-list>-->
+    <!--<v-list-tile-->
+    <!--value="true"-->
+    <!--v-for="(item, i) in items"-->
+    <!--:key="i"-->
+    <!--&gt;-->
+    <!--<v-list-tile-action>-->
+    <!--<v-icon v-html="item.icon"></v-icon>-->
+    <!--</v-list-tile-action>-->
+    <!--<v-list-tile-content>-->
+    <!--<v-list-tile-title v-text="item.title"></v-list-tile-title>-->
+    <!--</v-list-tile-content>-->
+    <!--</v-list-tile>-->
+    <!--</v-list>-->
     <!--</v-navigation-drawer>-->
     <v-toolbar
       app
@@ -34,6 +34,12 @@
       </v-toolbar-side-icon>
       <v-toolbar-title class="accent--text" v-text="title"></v-toolbar-title>
       <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn v-show="cart.length > 0" flat @click.stop="goCart">
+          <v-icon>shopping_cart</v-icon>
+          <v-card-text>{{ cart.length }}</v-card-text>
+        </v-btn>
+      </v-toolbar-items>
     </v-toolbar>
     <v-content>
       <router-view/>
@@ -45,27 +51,35 @@
 </template>
 
 <script>
-export default {
-  data () {
-    return {
-      clipped: false,
-      drawer: true,
-      fixed: false,
-      items: [{
-        icon: 'bubble_chart',
+  export default {
+    data() {
+      return {
+        clipped: false,
+        drawer: true,
+        fixed: false,
+        items: [{
+          icon: 'bubble_chart',
+          title: 'Voltz'
+        }],
+        miniVariant: false,
+        right: true,
+        rightDrawer: false,
         title: 'Voltz'
-      }],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Voltz'
-    }
-  },
-  methods: {
-    goAdmin () {
-      this.$router.push({ name: "Admin" });
-    }
-  },
-  name: 'App'
-}
+      }
+    },
+    methods: {
+      goAdmin() {
+        this.$router.push({name: "Admin"});
+      },
+      goCart() {
+        this.$router.push({name: "Cart"})
+      }
+    },
+    computed: {
+      cart () {
+        return this.$store.getters.cart
+      }
+    },
+    name: 'App'
+  }
 </script>

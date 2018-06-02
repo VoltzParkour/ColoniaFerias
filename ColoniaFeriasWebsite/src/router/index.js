d6673c6543e557ae7d70b86905ae2d27bd4c2afa
fernando.motta <fernando.motta@wikki.com.br>
2018-06-02 13:38:34 -0300

    Bug with links from admin page solved.

--- ColoniaFeriasWebsite/src/router/index.js
+++ ColoniaFeriasWebsite/src/router/index.js
@@ -49,13 +49,13 @@ let router = new Router({
   ]
 })
 
-// router.beforeEach((to, from, next) => {
-//   let currentUser = firebase.auth().currentUser;
-//   let requiresAuth = to.matched.some(record => record.meta.requiresAuth);
-//
-//   if (requiresAuth && !currentUser) next('login')
+ router.beforeEach((to, from, next) => {
+   let currentUser = firebase.auth().currentUser;
+   let requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+
+   if (requiresAuth && !currentUser) next('login')
 //   else if (!requiresAuth && currentUser) next('admin')
-//   else next()
-// })
+   else next()
+ })
 
 export default router

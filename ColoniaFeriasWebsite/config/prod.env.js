f924ada15b71a4c039085b47e1aec097855f1bfc
Luis Felipe Nicolau <luisfnicolau@hotmail.com>
2018-12-16 17:19:01 -0400

    Change api url to ip address

--- ColoniaFeriasWebsite/config/prod.env.js
+++ ColoniaFeriasWebsite/config/prod.env.js
@@ -1,5 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  ROOT_API: '"http://colonia.voltzparkour.com.br/api/"'
+  // ROOT_API: '"http://colonia.voltzparkour.com.br/api/"'
+  ROOT_API: '"http://159.203.67.240/api/"'
 }
